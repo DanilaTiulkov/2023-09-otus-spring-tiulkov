f4496130b5e655bef56ad46ed9342301970d43ff
Danila <danila-tiulkov01@mail.ru>
2023-11-08 17:41:12 +0300

    Commit 08.11.23

--- student-test-p1/src/main/java/otus/ru/example/service/TestServiceImpl.java
+++ student-test-p1/src/main/java/otus/ru/example/service/TestServiceImpl.java
@@ -2,7 +2,8 @@ package otus.ru.example.service;
 
 
 import otus.ru.example.dao.QuestionDao;
-import otus.ru.example.model.Question;
+import otus.ru.example.domain.Answer;
+import otus.ru.example.domain.Question;
 import java.util.List;
 
 public class TestServiceImpl implements TestService {
@@ -20,12 +21,11 @@ public class TestServiceImpl implements TestService {
     public void executeTest() {
         List<Question> questions = questionDao.getQuestions();
         for (int count = 0; count < questions.size(); count++) {
-            String question = questions.get(count).getQuestion();
-            String firstAnswer = questions.get(count).getFirstAnswer();
-            String secondAnswer = questions.get(count).getSecondAnswer();
-            String thirdAnswer = questions.get(count).getThirdAnswer();
-            ioService.printFormattedLine("%d.%s\n %s\n %s\n %s",
-                    count + 1, question, firstAnswer, secondAnswer, thirdAnswer);
+            Question question = questions.get(count);
+            List<Answer> answers = question.answers();
+            String questionText = question.text();
+            ioService.printFormattedLine("%d.%s", count + 1, questionText);
+            answers.forEach(answer -> ioService.printLine(" " + answer.text()));
         }
     }
 }
