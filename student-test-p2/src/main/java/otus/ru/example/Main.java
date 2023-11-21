26adc0383a700a9d69dbec7793b45f334d097df0
Danila <danila-tiulkov01@mail.ru>
2023-11-21 03:59:05 +0300

    Commit 20.11.2023

--- student-test-p2/src/main/java/otus/ru/example/Main.java
+++ student-test-p2/src/main/java/otus/ru/example/Main.java
@@ -1,15 +1,20 @@
 package otus.ru.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import otus.ru.example.config.AppConfig;
 import otus.ru.example.service.TestRunnerService;
 
-
+@Configuration
+@ComponentScan("otus.ru.example")
+@PropertySource("classpath:application.properties")
 public class Main {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(Main.class);
         TestRunnerService testRunnerService = context.getBean("testRunnerService", TestRunnerService.class);
         testRunnerService.run();
     }
