f704a3136fdbabd4636134493e0a484be3016d8a
Danila <danila-tiulkov01@mail.ru>
2023-12-19 18:18:06 +0300

    Commit 19.12.2023

--- student-test-p4/src/test/java/otus/ru/example/dao/SpringBootTestConfiguration.java
+++ student-test-p4/src/test/java/otus/ru/example/dao/SpringBootTestConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 import otus.ru.example.config.AppConfig;
 
-@ComponentScan({"otus.ru.example.dao", "otus.ru.example.config", "otus.ru.example.service"}) // почему если убрать аннотацию @SpringBootConfiguration ничего не поменяется?
+@ComponentScan({"otus.ru.example.dao", "otus.ru.example.config", "otus.ru.example.service"})
 @EnableConfigurationProperties(AppConfig.class)
 @SpringBootConfiguration
 public class SpringBootTestConfiguration {
