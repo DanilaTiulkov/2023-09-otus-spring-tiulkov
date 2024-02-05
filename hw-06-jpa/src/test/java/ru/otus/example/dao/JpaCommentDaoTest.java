326a1fc5ca3917e9d19e273fbff774d33acb98d8
Danila <danila-tiulkov01@mail.ru>
2024-02-06 01:18:00 +0300

    Commit 06.02.2024

--- hw-06-jpa/src/test/java/ru/otus/example/dao/JpaCommentDaoTest.java
+++ hw-06-jpa/src/test/java/ru/otus/example/dao/JpaCommentDaoTest.java
@@ -2,7 +2,6 @@ package ru.otus.example.dao;
 
 
 import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
