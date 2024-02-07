9980058900f98fa6d474724902afd698ebf7603e
Danila <danila-tiulkov01@mail.ru>
2024-02-08 02:12:55 +0300

    Commit 08.02.2024

--- hw-07-springData/src/main/java/ru/otus/example/Application.java
+++ hw-07-springData/src/main/java/ru/otus/example/Application.java
@@ -3,17 +3,11 @@ package ru.otus.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.otus.example.dao.AuthorDao;
-import ru.otus.example.dao.BookDao;
-import ru.otus.example.dao.CommentDao;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
--- hw-07-springData/src/main/java/ru/otus/example/dao/GenreDao.java
+++ hw-07-springData/src/main/java/ru/otus/example/dao/GenreDao.java
@@ -3,8 +3,5 @@ package ru.otus.example.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.example.models.Genre;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface GenreDao extends JpaRepository<Genre, Long> {
 }
