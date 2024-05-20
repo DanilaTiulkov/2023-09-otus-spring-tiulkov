40f2263ad4498b8ba403636a9d6dfc7377c657f1
Danila <danila-tiulkov01@mail.ru>
2024-05-21 01:14:16 +0300

    Commit 20.05.2024

--- hw-17-Docker/src/main/resources/schema.sql
+++ hw-17-Docker/src/main/resources/schema.sql
@@ -2,10 +2,12 @@ create table Authors(
     author_id bigserial PRIMARY KEY,
     full_name varchar(255)
 );
+
 create table Genres(
     genre_id bigserial PRIMARY KEY,
     genre_name varchar(255)
 );
+
 create table Books(
     book_id bigserial PRIMARY KEY,
     title varchar(255),
@@ -14,12 +16,14 @@ create table Books(
     foreign key (author_id) references Authors(author_id) on delete cascade,
     foreign key (genre_id) references Genres(genre_id) on delete cascade
 );
+
 create table Comments(
     comment_id bigserial PRIMARY KEY,
     comment_text varchar(255),
     book_id bigint,
     foreign key (book_id) references Books(book_id) on delete cascade
 );
+
 create table Users(
     user_id bigserial PRIMARY KEY,
     username varchar(255),
