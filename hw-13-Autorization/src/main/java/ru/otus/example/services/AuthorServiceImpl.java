3c6b4db67d70d9043ddbcd6d22e368513f23090e
Danila <danila-tiulkov01@mail.ru>
2024-07-16 21:35:03 +0300

    Commit 16.07.2024

---
+++ hw-13-Autorization/src/main/java/ru/otus/example/services/AuthorServiceImpl.java
@@ -0,0 +1,27 @@
+package ru.otus.example.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.example.dao.AuthorDao;
+import ru.otus.example.models.Author;
+
+import java.util.List;
+
+@Service("authorService")
+public class AuthorServiceImpl implements  AuthorService {
+
+    private final AuthorDao jpaAuthorDao;
+
+    @Autowired
+    public AuthorServiceImpl(AuthorDao jpaAuthorDao) {
+        this.jpaAuthorDao = jpaAuthorDao;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Author> findAll() {
+        return jpaAuthorDao.findAll();
+    }
+}
